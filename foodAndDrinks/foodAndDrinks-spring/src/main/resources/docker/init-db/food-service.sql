6fa23ef3e40e4841c18e2b24da8651b790b1f5b0
Gitea <gitea@fake.local>
2021-12-23 16:13:09 +0100

    fix sql

--- foodAndDrinks/foodAndDrinks-spring/src/main/resources/docker/init-db/food-service.sql
+++ foodAndDrinks/foodAndDrinks-spring/src/main/resources/docker/init-db/food-service.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE  IF NOT EXISTS `service_food` CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER 'yokudlela'@'%' IDENTIFIED BY 'yokudlela';
-GRANT ALL PRIVILEGES ON `service_table`.* TO `yokudlela`@`%`;
+GRANT ALL PRIVILEGES ON `service_food`.* TO `yokudlela`@`%`;
